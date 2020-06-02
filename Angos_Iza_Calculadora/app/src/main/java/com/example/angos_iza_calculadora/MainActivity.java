b36d8dd450c206e638fb87dab4436941700f602f
Dennys Iza <dgiza@espe.edu.ec>
2020-06-02 17:10:55 -0500

    Calculadora v0.6
    Corrección de diseño y adición de botones

--- Angos_Iza_Calculadora/app/src/main/java/com/example/angos_iza_calculadora/MainActivity.java
+++ Angos_Iza_Calculadora/app/src/main/java/com/example/angos_iza_calculadora/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.angos_iza_calculadora;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.angos_iza_calculadora.databinding.ActivityMainBinding;
+
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=DataBindingUtil.setContentView(this, R.layout.activity_main);
+
     }
+    private double Valor1= Double.NaN;
+    private double Valor2;
+
+    private static final char ADDITION='+';
+    private static final char SUBSTRACTION='-';
+    private static final char MULTIPLY='X';
+    private static final char DIVISION='/';
+
+    private char CURRENT_ACTION;
+
+    DecimalFormat decimalFormat =new DecimalFormat("#.##########");
+
 }
