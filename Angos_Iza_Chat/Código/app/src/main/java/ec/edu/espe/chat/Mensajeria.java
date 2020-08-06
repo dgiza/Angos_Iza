f9ea35469817d39b820ec79586490b2e97199358
Dennys Iza <dgiza@espe.edu.ec>
2020-08-06 12:06:54 -0500

    Chat

---
+++ Angos_Iza_Chat/Código/app/src/main/java/ec/edu/espe/chat/Mensajeria.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ec.edu.espe.chat;
+
+public class Mensajeria {
+    /**
+     * Declaración de parámetros para los mensajes
+     */
+    private String id;
+    private String text;
+    private String name;
+    private String photoUrl;
+    private String imageUrl;
+    /**
+     * Sobre carga de constructores
+     * Tenemos un constructor por defecto y otro con atributos
+     */
+    public Mensajeria() {
+    }
+
+    /**
+     * Sobre carga de constructores
+     * Constructor con parámetros
+     * @param imageUrl
+     * @param name
+     * @param photoUrl
+     * @param text
+     *
+     */
+    public Mensajeria(String text, String name, String photoUrl, String imageUrl) {
+        this.text = text;
+        this.name = name;
+        this.photoUrl = photoUrl;
+        this.imageUrl = imageUrl;
+    }
+    /**
+     * Métodos Getter y Setter
+     */
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
